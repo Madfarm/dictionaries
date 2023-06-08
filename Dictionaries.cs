ae8c6cbe9778c3c1a8a8159448605f2054fa9992
AnthonyScott <anthony.scott1005@gmail.com>
2023-06-08 19:48:48 -0400

    Create a dictionary

---
+++ Dictionaries.cs
@@ -0,0 +1,11 @@
+namespace Dictionaries;
+
+class NewThingy
+{
+    static void goDictionaryGo()
+    {
+        Dictionary<int, string> myDic = new Dictionary<int, string>();
+
+        myDic.Add(2, "raytrace");
+    }
+}
---
+++ notes.md
@@ -0,0 +1,4 @@
+namespaces can be declared like this, with no brackets
+namespace DogHatGuy;
+
+this will make the namespace contain all types in that file
